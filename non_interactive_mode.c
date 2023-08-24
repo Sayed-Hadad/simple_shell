9d763de0054c28eeb7f4688a9c9393bd4aed6f20
Sayed-Elhadad <anasesozaking@gmail.com>
2023-08-24 15:19:59 +0200

    Betty Fix

--- non_interactive_mode.c
+++ non_interactive_mode.c
@@ -1,23 +1,27 @@
 #include "shell.h"
 
-
+/**
+ *non_interactive_mode - Function that runs the shell no interactive
+ *
+ *Return: void
+ */
 void non_interactive_mode(void)
 {
- char *line;
- char **args;
- int status = -1;
+	char *line;
+	char **args;
+	int status = -1;
 
- do {
-  line = r_stream();
-  args = divide_line(line); /* tokenize line */
-  status = exec_args(args);
-  /* avoid memory leaks */
-  free(line);
-  free(args);
-  /* exit with status */
-  if (status >= 0)
-  {
-   exit(status);
-  }
- } while (status == -1);
+	do {
+		line = r_stream();
+		args = divide_line(line); /*tokenize line */
+		status = exec_args(args);
+
+		free(line);
+		free(args);
+
+		if (status >= 0)
+		{
+			exit(status);
+		}
+	} while (status == -1);
 }
